51e04647d6dcf2c9a4e3c2dba283fe3ca96fa48c
Raj Patel <patelraj200208@gmail.com>
2024-05-15 22:18:00 +0530

    Gym_Webpage_React

---
+++ src/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Body from '../components/Body/Body'
+
+
+function Home() {
+  return (
+    <div className='w-11/12 mx-auto sm:mt-28 mt-14'>
+      <Header/>
+      <Body/>
+      
+    </div>
+  )
+}
+
+export default Home
